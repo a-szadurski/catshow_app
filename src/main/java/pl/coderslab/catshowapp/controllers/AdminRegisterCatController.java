9395c35067b219d7ee683bd3b99ee4f1c8ce152d
a-szadurski <szadurski.arkadiusz@gmail.com>
2021-03-16 15:03:39 +0100

    register cat half works

--- src/main/java/pl/coderslab/catshowapp/controllers/AdminRegisterCatController.java
+++ src/main/java/pl/coderslab/catshowapp/controllers/AdminRegisterCatController.java
@@ -3,16 +3,40 @@ package pl.coderslab.catshowapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.coderslab.catshowapp.entities.Cat;
+import pl.coderslab.catshowapp.repositories.CatRepository;
+import pl.coderslab.catshowapp.repositories.ContestantRepository;
+import pl.coderslab.catshowapp.repositories.ExhibitorRepository;
+import pl.coderslab.catshowapp.services.RegisterCatCmd;
 
 @Controller
-@RequestMapping("/user/admin/register/cat")
+@RequestMapping("/user/register/cat")
 public class AdminRegisterCatController {
+
+    CatRepository catRepository;
+    ExhibitorRepository exhibitorRepository;
+    ContestantRepository contestantRepository;
+
+    public AdminRegisterCatController(CatRepository catRepository, ExhibitorRepository exhibitorRepository,
+                                      ContestantRepository contestantRepository) {
+        this.catRepository = catRepository;
+        this.exhibitorRepository = exhibitorRepository;
+        this.contestantRepository = contestantRepository;
+    }
 
     @GetMapping
     public String displayForm(Model model) {
-        model.addAttribute(new Cat());
-        return "user/admin/admin-register-cat";
+        model.addAttribute(new RegisterCatCmd());
+        return "user/user-register-cat";
+    }
+
+    @PostMapping
+    public String submitForm( RegisterCatCmd registerCatCmd) {
+
+        catRepository.save(registerCatCmd.getCat());
+        exhibitorRepository.save(registerCatCmd.getExhibitor());
+        return "user/cat-register-success";
+
     }
 }
--- src/main/java/pl/coderslab/catshowapp/entities/Judge.java
+++ src/main/java/pl/coderslab/catshowapp/entities/Judge.java
@@ -1,8 +1,15 @@
 package pl.coderslab.catshowapp.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
+@ToString
 public class Judge {
 
     @Id
